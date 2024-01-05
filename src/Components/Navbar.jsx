f383f27325245de9faef339c9e18fc5acc042a5f
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-01-05 01:57:24 +0100

    create a fonction toggle  :tada:

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import {FaBars, FaTimes} from "react-icons/fa"
 
 export const Navbar = () => {
+    const navRef = useRef();
+
+    const showNavbar = () =>{
+        navRef.current.classList.toggle("responsive_nav")
+    }
+
+
+
+
   return (
     <header>
         <h3>Logo</h3>
-        <nav>
+        <nav ref={navRef}>
             <a href="/#">Home</a>
             <a href="/#">My work</a>
             <a href="/#">Blog</a>
             <a href="/#">About me</a> 
+            <button>
+                <FaTimes/>
+            </button>
         </nav>
         <button>
-            
+            <FaBars/>
         </button>
     </header>
   )
